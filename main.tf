56e240f932b19e6ae5887264eed805bd315f8873
Wania1713 <127691025+Wania1713@users.noreply.github.com>
2023-05-14 21:35:43 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = "bucket-project1"
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.website.bucket}/*"
+        ]
+      }
+    ]
+  })
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.website.bucket
+}
